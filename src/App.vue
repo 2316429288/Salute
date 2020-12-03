5da1f509fc65eb938b4574a2acf6feac666c00e2
zywwj <2316429288@qq.com>
2020-12-03 12:22:49 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
 
+// 测试数据
+const currentUser: UserProps = {
+  isLogin: true,
+  name: "Eddie"
+};
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "Salute",
+    description: "我向你敬礼！Salute！",
+    avatar: "https://www.keaimeitu.com/uploads/allimg/20200207/22524615966.jpg"
+  },
+  {
+    id: 2,
+    title: "Salute",
+    description: "我向你敬礼！Salute！",
+    avatar:
+      "https://img01.dimtown.com/uploads/2020/07/1593862032-3c285582a5e54edca9bd1300b9689bdd400x400.jpg"
+  }
+];
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup() {
+    return {
+      list: testData,
+      currentUser
+    };
   }
 });
 </script>
